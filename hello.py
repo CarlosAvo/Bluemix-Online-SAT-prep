0a889d42ebf9635a5220f1904a7839be4e88fec4
CarlosAvo <CarlosAvo@users.noreply.github.com>
2015-04-19 18:54:28 -0400

    Added comments to hello.py

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ import random
 from wordnik import *
 apiUrl = 'http://api.wordnik.com/v4'
 ##API key goes here
-apiKey = ''
+apiKey = '1a238bb54cd623442091b118a4b0ceb8d325f2247a5c71ae8'
 client = swagger.ApiClient(apiKey, apiUrl)
 
 ##Call api to be used in WordApi
@@ -19,9 +19,11 @@ if os.getenv("VCAP_APP_PORT"):
 else:
     port = 8080
 
+
 @app.route("/")
+#Main menu from here you can access leaderboards and start the game
 def main():
-    
+    #CSS code for design and button layout
     cssCall="<HTML> <HEAD><TITLE>SAT Vocabulary</TITLE> <style media=\"screen\" type=\"text/css\"> #bigbutton {width:230px;\
 background: #00CC00; padding: 8px 14px 10px; border:1px solid #3e9cbf; cursor:pointer; font-size:1.5em;font-family:Oswald, sans-serif; \
 letter-spacing:.1em;text-shadow: 0 -1px 0px rgba(0, 0, 0, 0.3); color: #fff;-webkit-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 5px 0px 0px #205c73, 0px 10px 5px #999;\
@@ -37,12 +39,14 @@ box-shadow: inset 0px 1px 0px #3e9cbf, 0px 5px 0px 0px #205c73, 0px 10px 5px #99
 -moz-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 2px 0px 0px #205c73, 0px 2px 5px #999;box-shadow: inset 0px 1px 0px #3e9cbf, \
 0px 2px 0px 0px #205c73, 0px 2px 5px #999;}#leader{position: fixed;    left: 580px;    top: 350px;}</style> </HEAD><BODY bgcolor=\" #F1F1F1\">"
        
-        
+    #Returns website with buttons  
     return  cssCall+"<p style=\"text-align:center;font-size:35px;\">SAT Vocabulary Review<p/><form action=\"../Start\" method=\"POST\"><input id=\"bigbutton\" type=\"submit\" value=\"Start reviewing\" /></form><form action=\"../LeaderBoards\" ><input id=\"leader\" type=\"submit\" value=\"Leaderboards\" /></form><body/><html/> "
 
+
+#If leaderboard button clicked it redirects to that.  
 @app.route("/LeaderBoards")
 
-
+#Read file with names and scores and return them
 def leaderboards():
     showScore=open("LeaderbordScores.txt","r")
     cssCall="<HTML> <HEAD><TITLE>SAT Vocabulary</TITLE> <style media=\"screen\" type=\"text/css\"> #bigbutton {width:230px;\
@@ -54,6 +58,7 @@ letter-spacing:.1em;text-shadow: 0 -1px 0px rgba(0, 0, 0, 0.3); color: #fff;-web
 box-shadow: inset 0px 1px 0px #3e9cbf, 0px 2px 0px 0px #205c73, 0px 2px 5px #999;} #bigbutton{\
 position: fixed;    left: 580px;    top: 450px;} </style> </HEAD><BODY bgcolor=\" #F1F1F1\">"
     a=""
+    #Read ech line in the file and create a variable that saves each name and score with a break between names and cores 
     people=showScore.readlines()
     for i in people:
         a+=i+"<br/>"
@@ -62,9 +67,10 @@ position: fixed;    left: 580px;    top: 450px;} </style> </HEAD><BODY bgcolor=\
 
 
     
-
+#If start review button is clicked in main menu redirect here 
 @app.route("/Start", methods=['POST'])
 
+#Return definition for random word that must be guessed, it also shows a hint for said word
 def giveDef():
     
     tokens=['adversity', 'aesthetic', 'amicable', 'anachronistic', 'anecdote', 'anonymous', 'antagonist', 'arid', 'assiduous', 'asylum', 'benevolent', 'camaraderie', 'censure', 'circuitous', 'clairvoyant', 'collaborate', 'compassion', 'compromise', 'condescending', 'conditional', 'conformist', 'congregation', 'convergence', 'deleterious', 'demagogue', 'digression', 'diligent', 'discredit', 'disdain', 'divergent', 'emulate', 'enervating', 'enhance', 'ephemeral', 'evanescent', 'exasperation', 'exemplary', 'extenuating', 'florid', 'fortuitous', 'frugal', 'haughty', 'hedonist', 'hypothesis', 'impetuous', 'impute', 'incompatible', 'inconsequential', 'inevitable', 'integrity', 'intrepid', 'intuitive', 'jubilation', 'lobbyist', 'longevity', 'mundane', 'nonchalant', 'novice', 'opulent', 'orator', 'ostentatious', 'perfidious', 'precocious', 'pretentious', 'procrastinate', 'prosaic', 'prosperity', 'provocative', 'prudent', 'querulous', 'rancorous', 'reclusive', 'reconciliation', 'renovation', 'resilient', 'reverence', 'sagacity', 'scrutinize', 'spontaneity', 'spurious', 'submissive', 'substantiate', 'subtle', 'superficial', 'superfluous', 'suppress', 'surreptitious', 'tactful', 'tenacious', 'transient', 'venerable', 'vindicate', 'wary']
@@ -75,11 +81,10 @@ def giveDef():
 
     randomList=[]
 
-    ##Keep running as long as number of words in list are < 20
-
-    
+    #spot is a random position inside the list tokens, it chooses a random place that will be later indexed
     spot=random.randint(1,len(tokens))
 
+    #call api to get definition for a random word in token and save in variable definitions
     definitions = wordApi.getDefinitions(tokens[spot],sourceDictionaries='webster',limit=3)
 
 
@@ -88,23 +93,20 @@ def giveDef():
         pass
 
 
-        ## If randomW has a definition and related words then append to list
+        ## If it has a definition then append to list and remove from tokens so it doesn't repeat
     else:
         randomList.append(tokens[spot])
         tokens.remove(tokens[spot])
-            ##Make list global to call in other functions
-    definitions = wordApi.getDefinitions(randomList[0],sourceDictionaries='webster',limit=3)
 
 
-    ##Should run because None was purged in randomWords function
+    ##Should run because None was purged 
     if definitions is not None:
         ##Get the first definition from a list of definitions created from randomList[place]
+
         global randomDefinition
         randomDefinition=definitions[0].text
-
-        ## Print random definition that has to be solved
         
-
+        #CSS styling for website
         cssCall="<HTML> <HEAD><TITLE>SAT Vocabulary</TITLE> <style media=\"screen\" type=\"text/css\"> #bigbutton {width:230px;\
 background: #00CC00; padding: 8px 14px 10px; border:1px solid #3e9cbf; cursor:pointer; font-size:1.5em;font-family:Oswald, sans-serif; \
 letter-spacing:.1em;text-shadow: 0 -1px 0px rgba(0, 0, 0, 0.3); color: #fff;-webkit-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 5px 0px 0px #205c73, 0px 10px 5px #999;\
@@ -124,22 +126,23 @@ width: 275px;    border-radius: 10px;    -moz-border-radius: 10px; -webkit-borde
 -moz-box-shadow: 1px 1px 0 0 #FFF, 5px 5px 40px 2px #BBB inset;    -webkit-box-shadow: 1px 1px 0 0 #FFF, 5px 5px 40px 2px #BBB inset; -webkit-background-clip: padding-box;\
 outline: 0;</style> </HEAD><BODY bgcolor=\" #F1F1F1\">"
 
-        middle="<div id=\"show\" ><p style=\"position: fixed;left: 550px;top: 150px;font-size:25px;\">Hint: "+wordHintNoLetter(randomList[0])+"</p></div>"
-
-        other="<div id=\"hideaway\" style=\"display:none;\"><p style=\"position: fixed;left: 550px;top: 150px;font-size:25px;\"> Hint: "+wordHint(randomList[0])+"<p/></div>"
-
-        first="<p style=\"text-align:center;font-size:40px;\">"+randomDefinition+"<p/>"
+        #Hint that will be displayed,Hint button has to be pressed for the full hint to show.
+        hintFull="<div id=\"show\" ><p style=\"position: fixed;left: 550px;top: 150px;font-size:25px;\">Hint: "+wordHintNoLetter(randomList[0])+"</p></div>"
+        #Hint that shows the ammount of letters in word, after hint button is pressed, it disappears and is replaced with middle
+        hintPart="<div id=\"hideaway\" style=\"display:none;\"><p style=\"position: fixed;left: 550px;top: 150px;font-size:25px;\"> Hint: "+wordHint(randomList[0])+"<p/></div>"
+        #Definition for word is displayed in the center of the site
+        wordDefined="<p style=\"text-align:center;font-size:40px;\">"+randomDefinition+"<p/>"
         
         
 
-        
-        return  cssCall+first+middle+other+"<input id=\"hint\" type=\"submit\" value=\"Hint\" onClick=\"document.getElementById('hideaway').style.display=\'block\';document.getElementById('show').style.display='none'\"/><form action=\"../GiveAnswer\" method=\"POST\">\
+        #CSS HTML and python code is combined to display together
+        return  cssCall+wordDefined+hintFull+hintPart+"<input id=\"hint\" type=\"submit\" value=\"Hint\" onClick=\"document.getElementById('hideaway').style.display=\'block\';document.getElementById('show').style.display='none'\"/><form action=\"../GiveAnswer\" method=\"POST\">\
 <input type=\"text\" id=\"text\" name=\"answer\"/> <input id=\"bigbutton\" type=\"submit\" value=\"Submit answer\" /></form><body/><html/> "
 
-
+#After definition is submitted check to see if word is right or wrong
 @app.route("/GiveAnswer", methods=['POST'])
 
-def sayHello():
+def giveAnswer():
     cssCall="<HTML> <HEAD><TITLE>SAT Vocabulary</TITLE> <style media=\"screen\" type=\"text/css\"> .bigbutton {width:230px;\
 background: #00CC00; padding: 8px 14px 10px; border:1px solid #3e9cbf; cursor:pointer; font-size:1.5em;font-family:Oswald, sans-serif; \
 letter-spacing:.1em;text-shadow: 0 -1px 0px rgba(0, 0, 0, 0.3); color: #fff;-webkit-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 5px 0px 0px #205c73, 0px 10px 5px #999;\
@@ -150,18 +153,26 @@ position: fixed;    left: 580px;    top: 250px;} #text{position: fixed;left: 550
 width: 275px;    border-radius: 10px;    -moz-border-radius: 10px; -webkit-border-radius: 10px;    box-shadow: 1px 1px 0 0 #FFF, 5px 5px 40px 2px #BBB inset;\
 -moz-box-shadow: 1px 1px 0 0 #FFF, 5px 5px 40px 2px #BBB inset;    -webkit-box-shadow: 1px 1px 0 0 #FFF, 5px 5px 40px 2px #BBB inset; -webkit-background-clip: padding-box;\
 outline: 0; </style> </HEAD><BODY bgcolor=\" #F1F1F1\">"
+    #Request answer from website and save as variable
     someans= request.form['answer']
+
+    #If it's the same as the one in the global list then return a website saying that answer is correct and option to continue to another question 
     if someans == randomList[0]:
 
-        return cssCall+"<p style=\"text-align:center;font-size:35px;\">Answer is Correct!!!<p/><form action=\"../Restart\" method=\"POST\"><input type=\"submit\" class=\"bigbutton\" value=\"Continue\"\ style=\"\"/></form>"
-    
+        return cssCall+"<p style=\"text-align:center;font-size:35px;\">Answer is Correct!!!<p/><form action=\"../Start\" method=\"POST\"><input type=\"submit\" class=\"bigbutton\" value=\"Continue\"\ style=\"\"/></form>"
+    #If it's not the correct answer display the correct one and button displayed redirects to page where same definition is given
+    #to see if player remembers the mistake he made
     else:
         return cssCall+"<p style=\"text-align:center;font-size:35px;\"> Answer is incorrect, right answer is: <em>"+randomList[0]+"</em> <p/> "+"<form action=\"../Redo\" method=\"POST\"><input type=\"submit\" class=\"bigbutton\" value=\"Try Again\"\/></form><body/><html/>"
 
+
 @app.route("/Redo", methods=['POST'])
 
+#If answer was wrong show player the definition they got wrong and make player re-input answer
 def redo():
+    #get definitions for mistaken word again
     definitions = wordApi.getDefinitions(randomList[0],sourceDictionaries='webster',limit=3)
+    #Store only one of the definitions in a variable
     randomDefinition=definitions[0].text
     cssCall="<HTML> <HEAD><TITLE>SAT Vocabulary</TITLE> <style media=\"screen\" type=\"text/css\"> #bigbutton {width:230px;\
 background: #00CC00; padding: 8px 14px 10px; border:1px solid #3e9cbf; cursor:pointer; font-size:1.5em;font-family:Oswald, sans-serif; \
@@ -182,93 +193,21 @@ width: 275px;    border-radius: 10px;    -moz-border-radius: 10px; -webkit-borde
 -moz-box-shadow: 1px 1px 0 0 #FFF, 5px 5px 40px 2px #BBB inset;    -webkit-box-shadow: 1px 1px 0 0 #FFF, 5px 5px 40px 2px #BBB inset; -webkit-background-clip: padding-box;\
 outline: 0;</style> </HEAD><BODY bgcolor=\" #F1F1F1\">"
 
-    middle="<div id=\"show\" ><p style=\"position: fixed;left: 550px;top: 150px;font-size:25px;\">Hint: "+wordHintNoLetter(randomList[0])+"</p></div>"
-
-    other="<div id=\"hideaway\" style=\"display:none;\"><p style=\"position: fixed;left: 550px;top: 150px;font-size:25px;\"> Hint: "+wordHint(randomList[0])+"<p/></div>"
-
-    first="<p style=\"text-align:center;font-size:40px;\">"+randomDefinition+"<p/>"
+    #Hint that will be displayed,Hint button has to be pressed for the full hint to show.
+    hintFull="<div id=\"show\" ><p style=\"position: fixed;left: 550px;top: 150px;font-size:25px;\">Hint: "+wordHintNoLetter(randomList[0])+"</p></div>"
+    #Hint that shows the ammount of letters in word, after hint button is pressed, it disappears and is replaced with middle
+    hintPart="<div id=\"hideaway\" style=\"display:none;\"><p style=\"position: fixed;left: 550px;top: 150px;font-size:25px;\"> Hint: "+wordHint(randomList[0])+"<p/></div>"
+    #Definition for word is displayed in the center of the site
+    wordDefined="<p style=\"text-align:center;font-size:40px;\">"+randomDefinition+"<p/>"
         
         
 
-        
-    return  cssCall+first+middle+other+"<input id=\"hint\" type=\"submit\" value=\"Hint\" onClick=\"document.getElementById('hideaway').style.display=\'block\';document.getElementById('show').style.display='none'\"/><form action=\"../GiveAnswer\" method=\"POST\">\
+    #CSS HTML and python code is combined to display together
+    return  cssCall+wordDefined+hintFull+hintPart+"<input id=\"hint\" type=\"submit\" value=\"Hint\" onClick=\"document.getElementById('hideaway').style.display=\'block\';document.getElementById('show').style.display='none'\"/><form action=\"../GiveAnswer\" method=\"POST\">\
 <input type=\"text\" id=\"text\" name=\"answer\"/> <input id=\"bigbutton\" type=\"submit\" value=\"Submit answer\" /></form><body/><html/> "
 
 
-@app.route("/Restart", methods=['POST'])
-def restart():
-    tokens=['adversity', 'aesthetic', 'amicable', 'anachronistic', 'anecdote', 'anonymous', 'antagonist', 'arid', 'assiduous', 'asylum', 'benevolent', 'camaraderie', 'censure', 'circuitous', 'clairvoyant', 'collaborate', 'compassion', 'compromise', 'condescending', 'conditional', 'conformist', 'congregation', 'convergence', 'deleterious', 'demagogue', 'digression', 'diligent', 'discredit', 'disdain', 'divergent', 'emulate', 'enervating', 'enhance', 'ephemeral', 'evanescent', 'exasperation', 'exemplary', 'extenuating', 'florid', 'fortuitous', 'frugal', 'haughty', 'hedonist', 'hypothesis', 'impetuous', 'impute', 'incompatible', 'inconsequential', 'inevitable', 'integrity', 'intrepid', 'intuitive', 'jubilation', 'lobbyist', 'longevity', 'mundane', 'nonchalant', 'novice', 'opulent', 'orator', 'ostentatious', 'perfidious', 'precocious', 'pretentious', 'procrastinate', 'prosaic', 'prosperity', 'provocative', 'prudent', 'querulous', 'rancorous', 'reclusive', 'reconciliation', 'renovation', 'resilient', 'reverence', 'sagacity', 'scrutinize', 'spontaneity', 'spurious', 'submissive', 'substantiate', 'subtle', 'superficial', 'superfluous', 'suppress', 'surreptitious', 'tactful', 'tenacious', 'transient', 'venerable', 'vindicate', 'wary']
-
-    
-    #Empty list were all random words will be appended
-    global randomList
-
-    randomList=[]
-
-    ##Keep running as long as number of words in list are < 20
-
-    
-    spot=random.randint(1,len(tokens))
-
-    definitions = wordApi.getDefinitions(tokens[spot],sourceDictionaries='webster',limit=3)
-
-
-        ## If value of definition is None then skip that word
-    if definitions is None :
-        pass
-
-
-        ## If randomW has a definition and related words then append to list
-    else:
-        randomList.append(tokens[spot])
-        tokens.remove(tokens[spot])
-            ##Make list global to call in other functions
-    definitions = wordApi.getDefinitions(randomList[0],sourceDictionaries='webster',limit=3)
-
-
-    ##Should run because None was purged in randomWords function
-    if definitions is not None:
-        ##Get the first definition from a list of definitions created from randomList[place]
-        global randomDefinition
-        randomDefinition=definitions[0].text
-
-        ## Print random definition that has to be solved
-        cssCall="<HTML> <HEAD><TITLE>SAT Vocabulary</TITLE> <style media=\"screen\" type=\"text/css\"> #bigbutton {width:230px;\
-background: #00CC00; padding: 8px 14px 10px; border:1px solid #3e9cbf; cursor:pointer; font-size:1.5em;font-family:Oswald, sans-serif; \
-letter-spacing:.1em;text-shadow: 0 -1px 0px rgba(0, 0, 0, 0.3); color: #fff;-webkit-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 5px 0px 0px #205c73, 0px 10px 5px #999;\
--moz-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 5px 0px 0px #00CC00, 0px 10px 5px #999;box-shadow: inset 0px 1px 0px #3e9cbf, 0px 5px 0px 0px #205c73, 0px 10px 5px #999;\
--moz-border-radius: 10px;-webkit-border-radius: 10px;border-radius: 10px;}#bigbutton:hover, #bigbutton:focus {color:#dfe7ea;-webkit-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 2px 0px 0px #205c73, 0px 2px 5px #999;\
--moz-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 2px 0px 0px #205c73, 0px 2px 5px #999;box-shadow: inset 0px 1px 0px #3e9cbf, 0px 2px 0px 0px #205c73, 0px 2px 5px #999;} #bigbutton{\
-position: fixed;    left: 580px;    top: 250px;} \
-#hint {width:80px;background: #00CC00; padding: 8px 10px 8px; border:1px solid #3e9cbf; cursor:pointer; font-size:1.5em;\
-font-family:Oswald, sans-serif; letter-spacing:.1em;text-shadow: 0 -1px 0px rgba(0, 0, 0, 0.3); color: #fff;\
--webkit-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 5px 0px 0px #205c73, 0px 10px 5px #999;-moz-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 5px 0px 0px #205c73, 0px 10px 5px #999;\
-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 5px 0px 0px #205c73, 0px 10px 5px #999;-moz-border-radius: 10px;-webkit-border-radius: 10px;\
-border-radius: 10px;}#hint:hover, #hint:focus {color:#dfe7ea;-webkit-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 2px 0px 0px #205c73, 0px 2px 5px #999;\
--moz-box-shadow: inset 0px 1px 0px #3e9cbf, 0px 2px 0px 0px #205c73, 0px 2px 5px #999;box-shadow: inset 0px 1px 0px #3e9cbf, 0px 2px 0px 0px #205c73, 0px 2px 5px #999;}\
-#hint{position: fixed;    left: 900px;    top: 175px;}\
-#text{position: fixed;left: 550px;top: 220px;padding: 1px 20px; border: 0;    height: 25px;\
-width: 275px;    border-radius: 10px;    -moz-border-radius: 10px; -webkit-border-radius: 10px;    box-shadow: 1px 1px 0 0 #FFF, 5px 5px 40px 2px #BBB inset;\
--moz-box-shadow: 1px 1px 0 0 #FFF, 5px 5px 40px 2px #BBB inset;    -webkit-box-shadow: 1px 1px 0 0 #FFF, 5px 5px 40px 2px #BBB inset; -webkit-background-clip: padding-box;\
-outline: 0;</style> </HEAD><BODY bgcolor=\" #F1F1F1\">"
-
-    middle="<div id=\"show\" ><p style=\"position: fixed;left: 550px;top: 150px;font-size:25px;\">Hint: "+wordHintNoLetter(randomList[0])+"</p></div>"
-
-    other="<div id=\"hideaway\" style=\"display:none;\"><p style=\"position: fixed;left: 550px;top: 150px;font-size:25px;\"> Hint: "+wordHint(randomList[0])+"<p/></div>"
-
-    first="<p style=\"text-align:center;font-size:40px;\">"+randomDefinition+"<p/>"
-        
-        
-
-        
-    return  cssCall+first+middle+other+"<input id=\"hint\" type=\"submit\" value=\"Hint\" onClick=\"document.getElementById('hideaway').style.display=\'block\';document.getElementById('show').style.display='none'\"/><form action=\"../GiveAnswer\" method=\"POST\">\
-<input type=\"text\" id=\"text\" name=\"answer\"/> <input id=\"bigbutton\" type=\"submit\" value=\"Submit answer\" /></form><body/><html/> "
-
-
-
-
-    
-
+#Gives a hint of the correct word with some letters    
 def wordHint(word):
 
     ##List of all letters in alphabet to replace with _ _
@@ -313,6 +252,7 @@ def wordHint(word):
     ##Print newWord created s_o_ e-_ _ _ _ _ (stone-sober)
     return newWord
 
+#Gives hint without any letters only spaces and number of letters
 def wordHintNoLetter(word):
 
     ##List of all letters in alphabet to replace with _ _
